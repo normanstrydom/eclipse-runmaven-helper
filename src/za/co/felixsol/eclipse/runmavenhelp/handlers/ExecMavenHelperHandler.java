ad19f18e6ec36e702d077f2551c6d170d4a83eb5
normanstrydom <normans@felixsol.co.za>
2025-04-10 00:44:22 +0200

    pom located to process

--- src/za/co/felixsol/eclipse/runmavenhelp/handlers/ExecMavenHelperHandler.java
+++ src/za/co/felixsol/eclipse/runmavenhelp/handlers/ExecMavenHelperHandler.java
@@ -7,145 +7,84 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.ITreeSelection;
 import org.eclipse.jface.viewers.TreePath;
-import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.ui.navigator.resources.ProjectExplorer;
 
 public class ExecMavenHelperHandler extends AbstractHandler {
+
+	private IViewPart getProjectExplorerView() {
+		return PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
+				.findView("org.eclipse.ui.navigator.ProjectExplorer");
+	}
+
+	private void processSelection(ExecutionEvent event, ITreeSelection selection, Object selectedItem)
+			throws CoreException {
+		IResource pom = findPomToProcess(event, selection, selectedItem);
+		if (pom != null) {
+			MyLogger.logInfo("file:" + pom);
+		}
+	}
+
+	private IResource findPomToProcess(ExecutionEvent event, ITreeSelection selection, Object selectedItem)
+			throws CoreException {
+		TreePath[] paths = selection.getPathsFor(selectedItem);
+		if (paths.length > 0) {
+			TreePath path = paths[paths.length - 1];
+			for (int i = 0; i < path.getSegmentCount(); i++) {
+				Object item = path.getSegment(path.getSegmentCount() - i - 1);
+				if (item instanceof IContainer) {
+					IContainer container = (IContainer) item;
+					if (container.members() != null) {
+						for (IResource resource : container.members()) {
+							if (resource instanceof IFile) {
+								IFile file = (IFile) resource;
+								if ("pom.xml".equals(file.getName())) {
+									return resource;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
-//		IViewPart viewPart = PlatformUI.getWorkbench()
-//			    .getActiveWorkbenchWindow()
-//			    .getActivePage()
-//			    .findView("org.eclipse.ui.navigator.ProjectExplorer");
-
-		IViewPart viewPart;
-		ISelection selection = null;
-		try {
-			viewPart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
-					.findView("org.eclipse.ui.navigator.ProjectExplorer");
-//			    .findView("org.eclipse.jdt.ui.PackageExplorer");
-
-			selection = viewPart.getSite().getSelectionProvider().getSelection();
-			
-			MyLogger.logInfo("viewPart.getSite().getPart()=" + viewPart.getSite().getPart());
-//			viewPart.getSite().getPart().setFocus();
-			
-			MyLogger.logInfo("Selection - " + selection);
-
-			boolean showTree = false;
-			boolean showChildren = true;
-
-			if (selection != null) {
-				MyLogger.logInfo("Selection class - " + selection.getClass());
-				ITreeSelection ts = (ITreeSelection) selection;
-				
-				if (showChildren) {
-					Iterator iterator = ts.iterator();
-					while (iterator.hasNext()) {
-						Object obj = iterator.next();
-						MyLogger.logInfo("Obj - " + obj + ":" + obj.getClass());
-						TreePath[] paths = ts.getPathsFor(obj);
-						for (int i=0; i<paths.length; i++) {
-							MyLogger.logInfo("path - " + i+ ":" + paths[i] + ":" + paths[i].getClass());
-							MyLogger.logInfo("segmentcount = " + paths[i].getSegmentCount());
-							for (int j=0; j<paths[i].getSegmentCount(); j++) {
-								MyLogger.logInfo("segment " + j + ":" + paths[i].getSegment(j));
-								MyLogger.logInfo("\tclass " + j + ":" + paths[i].getSegment(j).getClass());
-								
-								if (paths[i].getSegment(j) instanceof IContainer) {
-									MyLogger.logInfo("\t\tcontainer");
-									IContainer container = (IContainer)paths[i].getSegment(j);
-									if (container.members() !=null) {
-										MyLogger.logInfo("\t\t\tmembers" + container.members().length);
-										for (int k =0; k<container.members().length; k++) {
-											MyLogger.logInfo("\t\t\t\tmember=" + container.members()[k]); 
-											MyLogger.logInfo("\t\t\t\t\tis IContainer=" + (container.members()[k] instanceof IContainer)); 
-										}
-									}
-								}
-								
-							}
-						}
+		IViewPart viewPart = getProjectExplorerView();
+		if (viewPart != null) {
+			ISelection selection = viewPart.getSite().getSelectionProvider().getSelection();
+			if ((selection != null) && (selection instanceof ITreeSelection)) {
+				Iterator iterator = ((ITreeSelection) selection).iterator();
+				while (iterator.hasNext()) {
+					try {
+						processSelection(event, (ITreeSelection) selection, iterator.next());
+					} catch (CoreException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
 				}
-
-				if (showTree) {
-					Iterator iterator = ts.iterator();
-					while (iterator.hasNext()) {
-						Object obj = iterator.next();
-						MyLogger.logInfo("Obj - " + obj + ":" + obj.getClass());
-						TreePath[] paths = ts.getPathsFor(obj);
-						for (TreePath path : paths) {
-							MyLogger.logInfo("Path - " + path);
-							MyLogger.logInfo("Path.segCount - " + path.getSegmentCount());
-							for (int i = 0; i < path.getSegmentCount(); i++) {
-								MyLogger.logInfo("Seg - " + i + "=" + path.getSegment(i));
-								MyLogger.logInfo("Seg class - " + i + "=" + path.getSegment(i).getClass());
-
-								if (path.getSegment(i) instanceof IFile) {
-									IFile file = (IFile) path.getSegment(i);
-									MyLogger.logInfo("File name : " + file.getName());
-									MyLogger.logInfo("File location : " + file.getLocation());
-								}
-
-							}
-						}
-					}
-				}
-
-//				TreePath[] paths = ts.getPaths();
-//				if (paths != null) {
-//					for (TreePath path : paths) {
-//						MyLogger.logInfo("Path - " + path + ":" + path.getFirstSegment());
-//						MyLogger.logInfo("ParentPath - " + path.getParentPath());
-//						while (path != null) {
-//							MyLogger.logInfo(" ** Path - " + path + ":" + path);
-//							if ((path != null) && (path.getSegmentCount() > 0)) {
-//								for (int i = 0; i < path.getSegmentCount(); i++) {
-//									MyLogger.logInfo("  ** Segment("+ i + ") : " + path.getSegment(i));
-//									MyLogger.logInfo("  	*** Segment("+ i + ") : " + path.getSegment(i).getClass());
-//								}
-//							}
-//							path = path.getParentPath();
-//						}
-//					}
-//				}
 			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} catch (Throwable e) {
-			e.printStackTrace();
+		} else {
+			showMessage(event, "Project explorer not available.");
 		}
 
-//		MyLogger.logInfo("Logggggggggtest!");
-//
-//		IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
-//		System.out.println(projects);
-//		if (projects != null) {
-//			System.out.println(projects.length);
-//			for (IProject project : projects) {
-//				System.out.println(project.getName());
-//			}
-//
-//		}
-
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(window.getShell(), "CustomCommandTest",
-				"Hello, Eclipse world!! - " + selection.toString());
 		return null;
 	}
+
+	private void showMessage(ExecutionEvent event, String message) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(window.getShell(), "ExecMavenHelper", message);
+	}
+
 }
